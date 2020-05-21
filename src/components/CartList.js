3d4742a27d86a6f5d5eea4ae94b0027fb0e58820
Natalia Sokolova <nataliya.a.sokolova@gmaol.com>
2020-05-21 11:55:44 -0700

    added functionality fot increment decrement remove otem

---
+++ src/components/CartList.js
@@ -0,0 +1,20 @@
+import  React,  {Component} from 'react';
+import CartItem from './Cart/CartItem';
+
+
+
+export default function CartList ({value}) {
+    const {cart} = value
+    
+     return(
+            <div className = "contaner-fluid">
+                {cart.map(item => {
+                    return <CartItem key ={item.id} item = {item} value  = {value}/>
+                })}
+             <h1>hello from  cartlist</h1>
+                        
+             </div>
+            
+
+        );
+    }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ProductProvider} from './context';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
+  <ProductProvider>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+       <App />
+    </Router>
+    
+  </React.StrictMode>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
